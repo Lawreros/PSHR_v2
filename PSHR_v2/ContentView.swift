db399bccb406ff766ba643e3a1e1b26e4ec514ff
Lawreros <lawreros@gmail.com>
2022-01-29 11:11:46 -0500

    Initial Commit

---
+++ PSHR_v2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PSHR_v2
+//
+//  Created by Ross on 1/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PSHR_v2/PSHR_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  PSHR_v2App.swift
+//  PSHR_v2
+//
+//  Created by Ross on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct PSHR_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
