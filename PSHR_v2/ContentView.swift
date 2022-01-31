6e827a4d56583711954739038cdaa98011bcbe28
Lawreros <lawreros@gmail.com>
2022-01-30 22:29:04 -0500

    test having multiple branches

--- PSHR_v2/ContentView.swift
+++ PSHR_v2/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world! This is a test!to")
+        Text("Hello, world! This is a test!")
             .padding()
     }
 }
