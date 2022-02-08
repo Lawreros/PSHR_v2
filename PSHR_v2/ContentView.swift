948aa35ff6bafe7127abd0a669e987514ad9414e
Lawreros <lawreros@gmail.com>
2022-02-07 19:49:10 -0500

    update ContentView, following https://www.novelbits.io/intro-ble-mobile-development-ios-part-2/

--- PSHR_v2/ContentView.swift
+++ PSHR_v2/ContentView.swift
@@ -7,10 +7,68 @@
 
 import SwiftUI
 
+//struct ContentView: View {
+//    var body: some View {
+//        Text("Hello, world! This is a test!")
+//            .padding()
+//    }
+//}
+
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello, world! This is a test!")
-            .padding()
+        VStack (spacing: 10) {
+
+            Text("Bluetooth Devices")
+                .font(.largeTitle)
+                .frame(maxWidth: .infinity, alignment: .center)
+            List() {
+                Text("placeholder 1")
+                Text("placeholder 2")
+            }.frame(height: 300)
+
+            Spacer()
+
+            Text("STATUS")
+                .font(.headline)
+
+            // Status goes here
+            Text("Bluetooth status here")
+                .foregroundColor(.red)
+
+            Spacer()
+
+            HStack {
+                VStack (spacing: 10) {
+                    Button(action: {
+                        print("Start Scanning")
+                    }) {
+                        Text("Start Scanning")
+                    }
+                    Button(action: {
+                        print("Stop Scanning")
+                    }) {
+                        Text("Stop Scanning")
+                    }
+                }.padding()
+
+                Spacer()
+
+                VStack (spacing: 10) {
+                    Button(action: {
+                        print("Start Advertising")
+                    }) {
+                        Text("Start Advertising")
+                    }
+                    Button(action: {
+                        print("Stop Advertising")
+                    }) {
+                        Text("Stop Advertising")
+                    }
+                }.padding()
+            }
+            Spacer()
+        }
     }
 }
 
